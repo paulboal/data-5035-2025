714163c7db118765dbe5ae0c61495c87304d2c3c
Paul Boal <paul.boal@gmail.com>
2025-04-06 23:05:40 -0500

    Adding Week 11 examples and exercises

--- dagster_demo/dept_budget.py
+++ dagster_demo/dept_budget.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import dagster as dg
 
+# Add pytest so we can setup tests
+import pytest
+
 
 # The employee asset has basic information about the employee and which department
 # they were in each year.
@@ -9,12 +12,43 @@ def employee():
     df = pd.read_csv('data/employee.csv')
     return df
 
+## Verify the employee information
+## 1. Expected number of records
+## 2. Year is numeric
+## 3. Year Range is 2000-2099
+## 4. Employee is numeric
+
+@pytest.fixture
+def test_employee():
+    return employee()
+
+def test_employee_count(test_employee):
+    assert(len(test_employee) == 12)
+
+def test_year_isnumeric(test_employee):
+    assert(test_employee['year'].dtype == int)
+
+def test_year_range(test_employee):
+    assert((test_employee['year'] >= 2000).all() and (test_employee['year'] < 2100).all())
+
+def test_empid_isnumeric(test_employee):
+    assert(test_employee['employee_id'].dtype == int)
+
 
 # The salary asset has the compensation information for each employee by year
 @dg.asset(group_name='Ingestion')
 def salary():
     df = pd.read_csv('data/salary.csv')
     return df
+
+## Test salary data
+## 1. Test year is numeric
+## 2. Test year is 2000-2099
+
+@pytest.fixture()
+
+def test_year_range():
+    return salary()
 
 
 # Finally, we have the department salary asset that summarizes salary information by department
